948eb924982be758204c510c50e6a7a8f8e46bd9
123abc1920 <123abc1920@mail.ru>
2026-03-19 01:22:50 +0300

    remove .idea

--- src/main/java/com/abc1920/Main.java
+++ src/main/java/com/abc1920/Main.java
@@ -2,6 +2,8 @@ package com.abc1920;
 
 import com.abc1920.domain.model.factory.AppFactory;
 
+// TODO: .idea убрфть
+// TODO: getAll перейти на списки
 public class Main {
     public static void main(String[] args) {
         AppFactory appFactory = new AppFactory();
