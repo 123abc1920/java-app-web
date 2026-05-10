030fd9c648434a3e64349d1bfa5d469c235aae3f
123abc1920 <123abc1920@mail.ru>
2026-05-10 15:35:37 +0300

    fix: fix russian alphabet

--- src/main/java/com/abc1920/presentation/servlet/BaseServlet.java
+++ src/main/java/com/abc1920/presentation/servlet/BaseServlet.java
@@ -3,7 +3,11 @@ package com.abc1920.presentation.servlet;
 import com.abc1920.domain.model.factory.AppFactory;
 import com.abc1920.usecases.facades.EventServiceDomain;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public abstract class BaseServlet extends HttpServlet {
     protected static final EventServiceDomain domain;
@@ -17,6 +21,14 @@ public abstract class BaseServlet extends HttpServlet {
 
         AppFactory appFactory = new AppFactory();
         domain = appFactory.createServlet();
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+        super.service(req, resp);
     }
 }
 
